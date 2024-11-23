1e6de1ff93677f7b6a7e1282df3b05c11296b737
DRAGDEJ <123367216+edgardkj@users.noreply.github.com>
2024-11-23 16:05:04 +0100

    welcome page done

--- src/views/components/Title.tsx
+++ src/views/components/Title.tsx
@@ -6,6 +6,10 @@ interface CustomTitleProps {
 	text: string
 }
 
+interface CustomDescriptionProps {
+	text: string
+}
+
 const CustomTitle: React.FC<CustomTitleProps> = ({ text }) => {
 	return (
 		<Typography
@@ -17,6 +21,24 @@ const CustomTitle: React.FC<CustomTitleProps> = ({ text }) => {
 				marginBottom: '20px',
 				textShadow: '0px 4px 8px rgba(0,0,0,0.5)',
 				color: '#FFFFFF'
+			}}>
+			{text}
+		</Typography>
+	)
+}
+
+export const CustomDescription: React.FC<CustomDescriptionProps> = ({ text }) => {
+	return (
+		<Typography
+			variant="h5"
+			sx={{
+				fontSize: '1.5rem',
+				textAlign: 'center',
+				marginBottom: '30px',
+				color: '#FFFFFF',
+				lineHeight: '1.8',
+				textShadow: '0px 2px 4px rgba(0,0,0,0.4)',
+				maxWidth: '600px'
 			}}>
 			{text}
 		</Typography>
--- src/views/welcomePage.tsx
+++ src/views/welcomePage.tsx
@@ -1,7 +1,7 @@
 import { Home as HomeIcon } from '@mui/icons-material'
 import { Box } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
-import CustomTitle from './components/title'
+import CustomTitle, { CustomDescription } from './components/title'
 import CustomButton from './components/Button'
 
 const WelcomePage = () => {
@@ -33,18 +33,13 @@ const WelcomePage = () => {
 			<CustomTitle text="🍕 Benvenuto nel Totem della Bontà! 🍔" />
 
 			{/* Descrizione Simpatica */}
-			<Box
-				sx={{
-					marginBottom: '40px',
-					textAlign: 'center',
-					maxWidth: '600px',
-					lineHeight: '1.6',
-					textShadow: '0px 2px 4px rgba(0,0,0,0.4)'
-				}}>
-				Ti trovi nel posto giusto per un'esperienza culinaria unica! <br />
-				Che tu sia uno chef stellato o un campione nel bruciare toast, qui troverai qualcosa di speciale. <br /> Premi il pulsante
-				qui sotto e preparati a un viaggio di sapori (e risate)!
-			</Box>
+
+			{/* Descrizione Simpatica */}
+			<CustomDescription
+				text="Ti trovi nel posto giusto per un'esperienza culinaria unica! 
+        Che tu sia uno chef stellato o un campione nel bruciare toast, qui troverai qualcosa di speciale. 
+        Premi il pulsante qui sotto e preparati a un viaggio di sapori (e risate)!"
+			/>
 
 			{/* Bottone per Proseguire */}
 			<CustomButton text="🍴 Inizia l'avventura 🍴" onClick={handleNavigate} />
